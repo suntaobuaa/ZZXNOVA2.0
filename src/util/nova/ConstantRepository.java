d2acc126df15ff132e3bfb0ad129d277164e1a34
57853 <57853@DESKTOP-M1UEL66>
2017-11-15 10:32:13 +0800

    增加了全局变量类

---
+++ src/util/nova/ConstantRepository.java
@@ -0,0 +1,16 @@
+package util.nova;
+
+import org.jgraph.graph.DefaultGraphModel;
+import component.nova.MyGraph;
+
+/**
+ * @ClassName:     ConstantRepository.java
+ * @Description:   Save some globle constant 
+ * @author         zhangzengxiao
+ * @version        V1.0  
+ * @Date           2017年11月15日 上午9:54:08 
+ */
+public class ConstantRepository {
+    public static MyGraph mygraph = new MyGraph(new DefaultGraphModel());
+
+}
