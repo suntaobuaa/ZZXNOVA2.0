538249b6496ccb3e62efc8ca831d95c21d43401d
57853 <57853@DESKTOP-M1UEL66>
2017-11-24 11:06:31 +0800

    更换了主题皮肤

--- src/gui/nova/AddEdgeDialog.java
+++ src/gui/nova/AddEdgeDialog.java
@@ -42,7 +42,9 @@ public class AddEdgeDialog extends JDialog {
         MyGraph mg = ConstantRepository.mygraph;
         //读取图上已经存在的组件名称
         for (CellView cv : mg.getGraphLayoutCache().getAllViews()) {
-            cellname.add((String) cv.getAllAttributes().get("name"));
+            if (cv.getAllAttributes().get("name") != null) {
+                cellname.add((String) cv.getAllAttributes().get("name"));
+            }
         }
         fromcell = new JComboBox(cellname.toArray());
         tocell = new JComboBox(cellname.toArray());
@@ -53,32 +55,34 @@ public class AddEdgeDialog extends JDialog {
             public void actionPerformed(ActionEvent e) {
                 // TODO Auto-generated method stub
                 //                //插入cell
-                if(fromcell.getSelectedItem()==null||tocell.getSelectedItem()==null){
+                if (fromcell.getSelectedItem() == null || tocell.getSelectedItem() == null) {
                     JOptionPane.showMessageDialog(null, "开始节点与结束节点不能为空");
-                }
-                else{
-                //UtilGUI.checkEqu(fromcell.getSelectedItem().toString(), tocell.getSelectedItem().toString());
+                } else {
+                    //UtilGUI.checkEqu(fromcell.getSelectedItem().toString(), tocell.getSelectedItem().toString());
                     DefaultEdge edge = new DefaultEdge();
-                //设置格式
-                int arrow = GraphConstants.ARROW_CLASSIC;
-                GraphConstants.setLineEnd(edge.getAttributes(), arrow);
-                GraphConstants.setEndFill(edge.getAttributes(), true);
-                for (CellView cv : mg.getGraphLayoutCache().getAllViews()) {
-                   if(cv.getAllAttributes().get("name")!=null && (cv.getAllAttributes().get("name").equals(fromcell.getSelectedItem().toString()))){
-                       edge.setSource(((DefaultMutableTreeNode) cv.getCell()).getChildAt(0));
-                       MyGraphConstants.setPartent(edge.getAttributes(), (String) cv.getAllAttributes().get("name"));
-                   }
-                   if(cv.getAllAttributes().get("name")!=null && (cv.getAllAttributes().get("name").equals(tocell.getSelectedItem().toString()))){
-                       edge.setTarget(((DefaultMutableTreeNode) cv.getCell()).getChildAt(0));
-                       MyGraphConstants.setChild(edge.getAttributes(),(String) cv.getAllAttributes().get("name"));
-                   }
-                }
-                mg.getGraphLayoutCache().insert(edge);
-                dispose();
+                    //设置格式
+                    int arrow = GraphConstants.ARROW_CLASSIC;
+                    GraphConstants.setLineEnd(edge.getAttributes(), arrow);
+                    GraphConstants.setEndFill(edge.getAttributes(), true);
+                    for (CellView cv : mg.getGraphLayoutCache().getAllViews()) {
+                        if (cv.getAllAttributes().get("name") != null
+                                && (cv.getAllAttributes().get("name").equals(fromcell.getSelectedItem().toString()))) {
+                            edge.setSource(((DefaultMutableTreeNode) cv.getCell()).getChildAt(0));
+                            MyGraphConstants.setPartent(edge.getAttributes(),
+                                    (String) cv.getAllAttributes().get("name"));
+                        }
+                        if (cv.getAllAttributes().get("name") != null
+                                && (cv.getAllAttributes().get("name").equals(tocell.getSelectedItem().toString()))) {
+                            edge.setTarget(((DefaultMutableTreeNode) cv.getCell()).getChildAt(0));
+                            MyGraphConstants.setChild(edge.getAttributes(), (String) cv.getAllAttributes().get("name"));
+                        }
+                    }
+                    mg.getGraphLayoutCache().insert(edge);
+                    dispose();
                 }
             }
         });
-        
+
         this.setLayout(null);
         from.setBounds(48, 21, 54, 15);
         this.add(from);
--- src/nova/main/NovaMain.java
+++ src/nova/main/NovaMain.java
@@ -1,7 +1,13 @@
 package nova.main;
 
+import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+
+import org.jvnet.substance.SubstanceLookAndFeel;
+import org.jvnet.substance.skin.OfficeBlue2007Skin;
+import org.jvnet.substance.skin.SubstanceOfficeBlue2007LookAndFeel;
 
 /**
  * @ClassName:     NovaMain.java
@@ -11,9 +17,23 @@ import javax.swing.UnsupportedLookAndFeelException;
  * @Date           2017年11月15日 上午9:56:34 
  */
 public class NovaMain {
-    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
-        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        MyFrame nova = new MyFrame();
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException{
+        UIManager.setLookAndFeel("com.birosoft.liquid.LiquidLookAndFeel");
+//        try { 
+//            UIManager.setLookAndFeel(new SubstanceOfficeBlue2007LookAndFeel()); 
+//            //JFrame.setDefaultLookAndFeelDecorated(true); 
+//            //JDialog.setDefaultLookAndFeelDecorated(true); 
+//            //SubstanceLookAndFeel.setCurrentTheme(new Substance); 
+////          SubstanceLookAndFeel.setSkin(new EmeraldDuskSkin()); 
+////          SubstanceLookAndFeel.setCurrentButtonShaper(new ClassicButtonShaper()); 
+////          SubstanceLookAndFeel.setCurrentWatermark(new SubstanceBubblesWatermark()); 
+////          SubstanceLookAndFeel.setCurrentBorderPainter(new StandardBorderPainter()); 
+////            SubstanceLookAndFeel.setCurrentGradientPainter(new StandardGradientPainter()); 
+////            SubstanceLookAndFeel.setCurrentTitlePainter(new FlatTitePainter()); 
+//        } catch (Exception e) { 
+//            System.err.println("Something went wrong!"); 
+//        } 
+        new MyFrame();
     }
 
 }
